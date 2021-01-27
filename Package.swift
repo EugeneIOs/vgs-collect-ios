9d4fefdc741f6d33270c1b613ddcaf86da83225d
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-01-27 09:00:12 +0200

    Add new target

--- Package.swift
+++ Package.swift
@@ -29,13 +29,17 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VGSCollectSDK",
-					  dependencies: ["VGSCollectSDK", "VGSCollectResources"],
+					  dependencies: [
+							.target(name: "VGSCollectResources")
+						],
   					exclude: [
 							"VGSCollectSDK.h",
 	 				]),
 			.target(
 					name: "VGSCollectSDK-Light",
-				  dependencies: ["VGSCollectSDK"]),
+				dependencies: [
+					.target(name: "VGSCollectSDK")
+				]),
 			.target(
 					name: "VGSCollectResources"),
 			.testTarget(
